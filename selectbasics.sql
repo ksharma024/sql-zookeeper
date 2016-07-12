5a08fc1e31b81c34dbc3fb8736d5296f2944ee94
Karn Sharma <karn.sharma024@gmail.com>
2016-07-12 14:56:44 -0400

    Update selectbasics.sql

--- selectbasics.sql
+++ selectbasics.sql
@@ -4,4 +4,15 @@ SELECT population FROM world
   WHERE name = 'Germany';
   
   
+SELECT name, population FROM world
+  WHERE name IN ('Iceland', 'Ireland', 'Denmark');
+  
+  
+SELECT name, area FROM world
+  WHERE area BETWEEN 250000 AND 300000 
+  
+ SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000 
+  
+  
   
