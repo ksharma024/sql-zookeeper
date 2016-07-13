fb25d8cbca941bf16a7645e331188a4fe1f3c794
Karn Sharma <karn.sharma024@gmail.com>
2016-07-13 09:43:07 -0400

    Create selectname.sql

---
+++ selectname.sql
@@ -0,0 +1,33 @@
+-- Select Name --
+
+SELECT name FROM world
+  WHERE name LIKE 'Y%';
+  
+SELECT name FROM world
+  WHERE name LIKE '%y';
+  
+SELECT name FROM world
+  WHERE name LIKE '%x%';
+  
+SELECT name FROM world
+  WHERE name LIKE '%land';
+  
+SELECT name FROM world
+  WHERE name LIKE 'C%' AND name LIKE '%ia';
+  
+SELECT name FROM world
+  WHERE name LIKE '%oo%';
+  
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%';
+  
+SELECT name FROM world
+ WHERE name LIKE '_t%';
+ 
+ SELECT name FROM world
+ WHERE name LIKE '%o__o%';
+ 
+SELECT name FROM world
+ WHERE name LIKE '____';
+ 
+ 
