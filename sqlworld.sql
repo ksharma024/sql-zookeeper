dec5f53c9f209037f0e637832366829ca3604b31
Karn Sharma <karn.sharma024@gmail.com>
2016-07-26 18:36:35 -0400

    Update sqlworld.sql

--- sqlworld.sql
+++ sqlworld.sql
@@ -17,5 +17,29 @@ FROM world
 WHERE name in ('France', 'Germany', 'Italy')
 
 
+SELECT name
+FROM world
+WHERE name LIKE '%united%'
+
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000
+
+SELECT name, population, area
+FROM world
+WHERE (area > 3000000 AND population < 250000000)
+  OR (area < 3000000 and population > 250000000)
+
+
+
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000, 2)
+FROM world
+WHERE continent = 'South America'
+
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp > 1000000000000
+
+
 
 
